bcee1cdfd2cbb9a419d7770cb06f64addfb848f2
Andrei <andrei.dumitran@gmail.com>
2021-12-30 21:52:38 +0000

    adding readme

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const request = require("postman-request");
 const url =
   "http://api.weatherstack.com/current?access_key=b2ac74b65923cece89b554d5cb948cf6&query=37.8267,-122.4233";
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ uri: url, json: true }, (error, response) => {
+  console.log(response.body.current);
 });
