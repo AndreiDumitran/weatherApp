6d92113d15833f16d08fdcf95629ba8a7995f55d
Andrei <andrei.dumitran@gmail.com>
2021-12-31 01:37:22 +0000

    added callback functions and basic functionality

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const request = require("postman-request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/weather");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=b2ac74b65923cece89b554d5cb948cf6&query=37.8267,-122.4233";
+const address = process.argv[2];
 
-request({ uri: url, json: true }, (error, response) => {
-  console.log(response.body.current);
+geocode(address, (error, { latitude, longitude, location } = {}) => {
+  if (error) {
+    return console.log(error);
+  }
+  forecast(latitude, longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(location);
+    console.log("Forecast", forecastData);
+  });
 });
---
+++ utils/weather.js
@@ -0,0 +1,28 @@
+const request = require("postman-request");
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=b2ac74b65923cece89b554d5cb948cf6&query=" +
+    longitude +
+    "," +
+    latitude +
+    "&units=m";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location, change it!", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees out. It ffels like " +
+          body.current.feelslike +
+          " degrees out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
